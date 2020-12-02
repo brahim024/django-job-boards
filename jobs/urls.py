a90f3131e0e61784967c7e1bc34b51e6fd5cdb0d
brahim024 <ifninos168@gmail.com>
2020-12-02 14:10:41 +0100

    add urls and filters

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,4 +3,6 @@ from .import views
 app_name='jobs'
 urlpatterns = [
     path('', views.all_post,name='all_post'),
+    path('add', views.add_job, name='add_job'),
+    path('<str:slug>', views.post, name='job_details'),
     ]
