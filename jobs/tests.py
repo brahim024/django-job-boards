52d4de98b31b59e6285b63a77ddacbe9667ca35e
brahim024 <ifninos168@gmail.com>
2020-12-02 18:54:18 +0100

    add testing home page

--- jobs/tests.py
+++ jobs/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+class HomePageTests(TestCase):
+	def test_home_page(self):
+		response=self.client.get('/')
+		self.assertEqual(response.status_code,404)
+	def test_home_page_url_name(self):
+		response=self.client.get(reverse('all_post'))
+		self.assertEqual(response.status_code,404)
