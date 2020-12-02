911b3417819559cb4dbcbef53f34a2f0e9db9245
brahim024 <ifninos168@gmail.com>
2020-12-02 19:25:42 +0100

    add login process

--- project/urls.py
+++ project/urls.py
@@ -18,5 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('login.urls',namespace='accounts')),
     path('jobs/',include('jobs.urls',namespace='jobs')),
 ]
